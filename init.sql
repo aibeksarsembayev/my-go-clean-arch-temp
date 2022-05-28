478d841c7f42db103c404c75cef020d2e88bfd83
quazar <s-bek-k@mail.ru>
2022-05-28 13:06:03 +0600

    sql update with my version

--- init.sql
+++ init.sql
@@ -1,70 +1,32 @@
--- CREATE TABLE IF NOT EXISTS "post" (
--- 	"id" SERIAL NOT NULL UNIQUE PRIMARY KEY,
--- 	"title" VARCHAR (50) NOT NULL,
--- 	"content" VARCHAR (255) NOT NULL,
--- 	"user" SERIAL NOT NULL REFERENCES "user" ("id") ON DELETE CASCADE,
--- 	"category" INT NOT NULL,
--- 	"created" TIMESTAMP NOT NULL,
--- 	"updated" TIMESTAMP NOT NULL	
--- );
-
--- CREATE TABLE IF NOT EXISTS "user" (
--- 	"id" SERIAL NOT NULL UNIQUE PRIMARY KEY,
--- 	"name" VARCHAR (50) NOT NULL UNIQUE,
--- 	"email" VARCHAR (50) NOT NULL UNIQUE,
--- 	"password" VARCHAR (255) NOT NULL,
--- 	"created" TIMESTAMP NOT NULL,
--- 	"updated" TIMESTAMP NOT NULL
--- );
-
 CREATE TABLE IF NOT EXISTS "user" (
-    "user_id" BIGSERIAL PRIMARY KEY,
-    "email" TEXT NOT NULL UNIQUE,
-    "username" VARCHAR(16) NOT NULL,
-    "password" TEXT NOT NULL,
-    "created_at" TIMESTAMP NOT NULL,
-    "updated_at" TIMESTAMP NOT NULL
+	"id" SERIAL NOT NULL UNIQUE PRIMARY KEY,
+	"name" VARCHAR (50) NOT NULL UNIQUE,
+	"email" VARCHAR (50) NOT NULL UNIQUE,
+	"password" VARCHAR (255) NOT NULL,
+	"created" TIMESTAMP NOT NULL,
+	"updated" TIMESTAMP NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "category" (
+    "id" SERIAL NOT NULL UNIQUE PRIMARY KEY,
+    "name" VARCHAR (255) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS "post" (
-    "post_id" BIGSERIAL PRIMARY KEY,
-    "user_id" BIGSERIAL NOT NULL REFERENCES "user" ("user_id") ON DELETE CASCADE,
-    "title" VARCHAR(128) NOT NULL,
-    "text" TEXT NOT NULL,
-    "created_at" TIMESTAMP NOT NULL,
-    "updated_at" TIMESTAMP NOT NULL
+	"id" SERIAL NOT NULL UNIQUE PRIMARY KEY,
+	"title" VARCHAR (50) NOT NULL,
+	"content" VARCHAR (255) NOT NULL,
+	"user_id" SERIAL NOT NULL REFERENCES "user" ("id") ON DELETE CASCADE,
+	"category_id" INT NOT NULL REFERENCES "category" ("id") ON DELETE CASCADE,
+	"created" TIMESTAMP NOT NULL,
+	"updated" TIMESTAMP NOT NULL	
 );
 
 CREATE TABLE IF NOT EXISTS "comment" (
-    "comment_id" BIGSERIAL PRIMARY KEY,
-    "user_id" BIGSERIAL NOT NULL REFERENCES "user" ("user_id") ON DELETE CASCADE,
-    "post_id" BIGSERIAL NOT NULL REFERENCES "post" ("post_id") ON DELETE CASCADE,
-    "text" TEXT NOT NULL,
-    "created_at" TIMESTAMP NOT NULL,
-    "updated_at" TIMESTAMP NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS "tag" (
-    "tag_id" BIGSERIAL PRIMARY KEY,
-    "name" TEXT NOT NULL UNIQUE
-);
-
-CREATE TABLE IF NOT EXISTS "post_tag" (
-    "post_tag_id" BIGSERIAL PRIMARY KEY,
-    "post_id" BIGSERIAL NOT NULL REFERENCES "post" ("post_id") ON DELETE CASCADE,
-    "tag_id" BIGSERIAL NOT NULL REFERENCES "tag" ("tag_id") ON DELETE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS "vote_post" (
-    "vote_post_id" BIGSERIAL PRIMARY KEY,
-    "user_id" BIGSERIAL NOT NULL REFERENCES "user" ("user_id") ON DELETE CASCADE,
-    "post_id" BIGSERIAL NOT NULL REFERENCES "post" ("post_id") ON DELETE CASCADE,
-    "vote" SMALLINT NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS "vote_comment" (
-    "vote_post_id" BIGSERIAL PRIMARY KEY,
-    "user_id" BIGSERIAL NOT NULL REFERENCES "user" ("user_id") ON DELETE CASCADE,
-    "comment_id" BIGSERIAL NOT NULL REFERENCES "comment" ("comment_id") ON DELETE CASCADE,
-    "vote" SMALLINT NOT NULL
+    "id" SERIAL NOT NULL UNIQUE PRIMARY KEY,
+    "content" VARCHAR (255) NOT NULL,
+    "post_id" SERIAL NOT NULL REFERENCES "post" ("id") ON DELETE CASCADE,
+    "user_id"  SERIAL NOT NULL REFERENCES "user" ("id") ON DELETE CASCADE,
+    "created" TIMESTAMP NOT NULL,
+	"updated" TIMESTAMP NOT NULL
 );
--- setup.sql
+++ setup.sql
@@ -1,3 +1,22 @@
+CREATE TABLE IF NOT EXISTS "user" (
+	"id" int NOT NULL,
+	"password" text NOT NULL,
+	"email" text NOT NULL,
+	"created" time NOT NULL,
+	"updated" time NOT NULL,
+	"name" text NOT NULL,
+	CONSTRAINT user_pk PRIMARY KEY ("id"),
+	CONSTRAINT user_un UNIQUE ("id","password","email")
+);
+
+CREATE TABLE IF NOT EXISTS "category" (
+	"id" int NOT NULL,
+	"name" text NOT NULL,
+	CONSTRAINT category_pk PRIMARY KEY ("id"),
+	CONSTRAINT category_un UNIQUE ("id"),
+	CONSTRAINT category_un UNIQUE ("name")
+);
+
 CREATE TABLE IF NOT EXISTS "post" (
 	"id" int NOT NULL,
 	"title" text NOT NULL,
@@ -7,16 +26,8 @@ CREATE TABLE IF NOT EXISTS "post" (
 	"updated" time NOT NULL,
 	"created" time NOT NULL,
 	CONSTRAINT post_pk PRIMARY KEY ("id"),
-	CONSTRAINT post_fk_1 FOREIGN KEY ("category") REFERENCES "category"("id"),
-	CONSTRAINT post_fk FOREIGN KEY ("user") REFERENCES "user"("id")
-);
-
-CREATE TABLE IF NOT EXISTS "category" (
-	"id" int NOT NULL,
-	"name" text NOT NULL,
-	CONSTRAINT category_pk PRIMARY KEY ("id"),
-	CONSTRAINT category_un UNIQUE ("id"),
-	CONSTRAINT category_un UNIQUE ("name")
+	CONSTRAINT post_fk_1 FOREIGN KEY ("category") REFERENCES "category"("id") ON DELETE CASCADE,
+	CONSTRAINT post_fk FOREIGN KEY ("user") REFERENCES "user"("id") ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS "comment" (
@@ -28,17 +39,7 @@ CREATE TABLE IF NOT EXISTS "comment" (
 	"updated" time NOT NULL,
 	CONSTRAINT comment_pk PRIMARY KEY ("id"),
 	CONSTRAINT comment_un UNIQUE ("id"),
-	CONSTRAINT comment_fk FOREIGN KEY ("post") REFERENCES "post"("id"),
-	CONSTRAINT comment_fk_1 FOREIGN KEY ("user") REFERENCES "user"("id")
+	CONSTRAINT comment_fk FOREIGN KEY ("post") REFERENCES "post"("id") ON DELETE CASCADE,
+	CONSTRAINT comment_fk_1 FOREIGN KEY ("user") REFERENCES "user"("id") ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS "user" (
-	"id" int NOT NULL,
-	"password" text NOT NULL,
-	"email" text NOT NULL,
-	"created" time NOT NULL,
-	"updated" time NOT NULL,
-	"name" text NOT NULL,
-	CONSTRAINT user_pk PRIMARY KEY ("id"),
-	CONSTRAINT user_un UNIQUE ("id","password","email")
-);
